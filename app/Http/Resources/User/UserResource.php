7e78d5673920200e2b6df05829b6119a7327663b
sh3ki <shekaigarcia@gmail.com>
2026-03-11 11:46:46 +0800

    Create app/Http/Resources/User/UserResource.php

---
+++ app/Http/Resources/User/UserResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use App\Http\Resources\UserRole\UserRoleResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        /** @var User $user */
+        $user = $this;
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'avatar' => $user->getAvatar(),
+            'gravatar' => $user->getGravatar(),
+            'skills' => $user->skills,
+            'role' => new UserRoleResource($user->userRole),
+            'role_id' => $user->role_id,
+            'status' => (bool) $user->status,
+            'permissions' => $user->permissions,
+            'dashboard_access' => $user->dashboard_access,
+            'effective_permissions' => $user->getEffectivePermissions(),
+            'permissions_map' => $user->getEffectivePermissionMap(),
+            'effective_dashboard_access' => $user->checkEffectiveDashboardAccess(),
+            'created_at' => $user->created_at->toISOString(),
+            'updated_at' => $user->updated_at->toISOString()
+        ];
+    }
+}
