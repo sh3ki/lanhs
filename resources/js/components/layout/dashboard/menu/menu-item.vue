57e0dc76073705d60088d1beee085d7bd643407a
sh3ki <shekaigarcia@gmail.com>
2026-04-15 04:18:23 +0800

    Update resources/js/components/layout/dashboard/menu/menu-item.vue

--- resources/js/components/layout/dashboard/menu/menu-item.vue
+++ resources/js/components/layout/dashboard/menu/menu-item.vue
@@ -10,7 +10,11 @@
                 :icon="icon"
                 class="mr-3 w-5 h-5 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
             ></svg-vue>
-            {{ label }}
+            <span>{{ label }}</span>
+            <span v-if="showDot" class="relative ml-auto flex h-2.5 w-2.5">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+                <span class="relative inline-flex h-2.5 w-2.5 rounded-full bg-red-600"></span>
+            </span>
         </a>
 
         <!-- Vue Router link -->
@@ -23,7 +27,11 @@
                 :icon="icon"
                 class="mr-3 w-5 h-5 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
             ></svg-vue>
-            {{ label }}
+            <span>{{ label }}</span>
+            <span v-if="showDot" class="relative ml-auto flex h-2.5 w-2.5">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+                <span class="relative inline-flex h-2.5 w-2.5 rounded-full bg-red-600"></span>
+            </span>
         </router-link>
     </li>
 </template>
@@ -49,6 +57,10 @@ export default {
             required: true,
         },
         external: {
+            type: Boolean,
+            default: false,
+        },
+        showDot: {
             type: Boolean,
             default: false,
         }
